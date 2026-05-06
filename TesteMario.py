00e37f3fd5b324877d72ea4fb0256acc42c6ac42
Lucas Oliveira <oliveira7github@gmail.com>
2026-05-05 21:27:50 -0300

    implementa entrada de dados utilizando estrutura while

--- TesteMario.py
+++ TesteMario.py
@@ -1,4 +1,18 @@
 G = []
 R = []
 
-print("Sistema de correção de provas iniciado")s
+print("Sistema de correção de provas iniciado")
+
+print('Digite o gabarito da prova (10 respostas):')
+cont = 0
+while cont < 10:
+    resposta = input(f'Questão {cont + 1}: ')
+    G.append(resposta)
+    cont = cont + 1
+
+print('Digite as respostas do aluno (10 respostas):')
+cont = 0
+while cont < 10:
+    resposta = input(f'Questão {cont + 1}: ')
+    R.append(resposta)
+    cont = cont + 1
